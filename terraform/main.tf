7ceac1aa34ad28591b9c39e56c910bda22da12cf
Christian Grøntved Andersen <christian.groentved@gmail.com>
2021-10-02 00:20:20 +0200

    Correction to main.tf based of old version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,39 @@
 resource "proxmox_vm_qemu" "k3s_server" {
-  count             = 1
-  name              = "kubernetes-master-${count.index}"
-  target_node       = "proxmox"
+  count       = 1
+  name        = "kubernetes-master-${count.index}"
+  target_node = "proxmox"
 
-  clone             = "ubuntu-2004-cloudinit-template"
+  clone = "ubuntu-2004-cloudinit-template"
 
-  os_type           = "cloud-init"
-  cores             = 2
-  sockets           = "1"
-  cpu               = "host"
-  memory            = 2048
-  scsihw            = "virtio-scsi-pci"
-  bootdisk          = "scsi0"
+  os_type  = "cloud-init"
+  cores    = 2
+  sockets  = "1"
+  cpu      = "host"
+  memory   = 2048
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
 
   disk {
-    id              = 0
-    size            = 20
-    type            = "scsi"
-    storage         = "local-lvm"
-    storage_type    = "lvm"
-    iothread        = true
+    size         = 20
+    type         = "scsi"
+    storage      = "local-lvm"
+    storage_type = "lvm"
+    iothread     = 1
   }
 
   network {
-    id              = 0
-    model           = "virtio"
-    bridge          = "vmbr0"
+    model  = "virtio"
+    bridge = "vmbr0"
   }
 
   lifecycle {
-    ignore_changes  = [
+    ignore_changes = [
       network,
     ]
   }
 
   # Cloud Init Settings
-  ipconfig0         = "ip=192.168.1.${count.index + 3}/24,gw=192.168.1.1"
+  ipconfig0 = "ip=192.168.1.${count.index + 3}/24,gw=192.168.1.1"
 
   sshkeys = <<EOF
   ${var.ssh_key}
@@ -43,43 +41,41 @@ resource "proxmox_vm_qemu" "k3s_server" {
 }
 
 resource "proxmox_vm_qemu" "k3s_agent" {
-  count             = 2
-  name              = "kubernetes-node-${count.index}"
-  target_node       = "proxmox"
+  count       = 2
+  name        = "kubernetes-node-${count.index}"
+  target_node = "proxmox"
 
-  clone             = "ubuntu-2004-cloudinit-template"
+  clone = "ubuntu-2004-cloudinit-template"
 
-  os_type           = "cloud-init"
-  cores             = 2
-  sockets           = "1"
-  cpu               = "host"
-  memory            = 2048
-  scsihw            = "virtio-scsi-pci"
-  bootdisk          = "scsi0"
+  os_type  = "cloud-init"
+  cores    = 2
+  sockets  = "1"
+  cpu      = "host"
+  memory   = 2048
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
 
   disk {
-    id              = 0
-    size            = 20
-    type            = "scsi"
-    storage         = "local-lvm"
-    storage_type    = "lvm"
-    iothread        = true
+    size         = 20
+    type         = "scsi"
+    storage      = "local-lvm"
+    storage_type = "lvm"
+    iothread     = 1
   }
 
   network {
-    id              = 0
-    model           = "virtio"
-    bridge          = "vmbr0"
+    model  = "virtio"
+    bridge = "vmbr0"
   }
 
   lifecycle {
-    ignore_changes  = [
+    ignore_changes = [
       network,
     ]
   }
 
   # Cloud Init Settings
-  ipconfig0         = "ip=192.168.1.${count.index + 4}/24,gw=192.168.1.1"
+  ipconfig0 = "ip=192.168.1.${count.index + 4}/24,gw=192.168.1.1"
 
   sshkeys = <<EOF
   ${var.ssh_key}
@@ -87,43 +83,41 @@ resource "proxmox_vm_qemu" "k3s_agent" {
 }
 
 resource "proxmox_vm_qemu" "storage" {
-  count             = 1
-  name              = "storage-node-${count.index}"
-  target_node       = "proxmox"
+  count       = 1
+  name        = "storage-node-${count.index}"
+  target_node = "proxmox"
 
-  clone             = "ubuntu-2004-cloudinit-template"
+  clone = "ubuntu-2004-cloudinit-template"
 
-  os_type           = "cloud-init"
-  cores             = 2
-  sockets           = "1"
-  cpu               = "host"
-  memory            = 2048
-  scsihw            = "virtio-scsi-pci"
-  bootdisk          = "scsi0"
+  os_type  = "cloud-init"
+  cores    = 2
+  sockets  = "1"
+  cpu      = "host"
+  memory   = 2048
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
 
   disk {
-    id              = 0
-    size            = 20
-    type            = "scsi"
-    storage         = "local-lvm"
-    storage_type    = "lvm"
-    iothread        = true
+    size         = 20
+    type         = "scsi"
+    storage      = "local-lvm"
+    storage_type = "lvm"
+    iothread     = 1
   }
 
   network {
-    id              = 0
-    model           = "virtio"
-    bridge          = "vmbr0"
+    model  = "virtio"
+    bridge = "vmbr0"
   }
 
   lifecycle {
-    ignore_changes  = [
+    ignore_changes = [
       network,
     ]
   }
 
   # Cloud Init Settings
-  ipconfig0         = "ip=192.168.1.${count.index + 6}/24,gw=192.168.1.1"
+  ipconfig0 = "ip=192.168.1.${count.index + 6}/24,gw=192.168.1.1"
 
   sshkeys = <<EOF
   ${var.ssh_key}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,15 +1,16 @@
 variable "pm_api_url" {
-  default = "https://"192.168.1.2":8006/api2/json"
+  default = "https://192.168.1.2:8006/api2/json"
 }
 
 variable "pm_user" {
-default = "root@pam"
+  default = "root@pam"
 }
 
 variable "pm_password" {
-default = "altan#10Knappedumrian"
+  default = "altan#10Knappedumrian"
 }
 
 variable "ssh_key" {
   default = "ssh-rsa WZADxvO5G19+6mAlEXOCScwvQDagfrMOonMYYUs024H3c60oqh3BvL8FGuphldPCgIfjkMfOo16yPssq63Gcj+LO7JOl4EAeDpbc6F2WTxoZGK0UuW6+mv5Btxzp/nMOn8ZZFjDzOrNY/6k6bYKLuVrzopIsMIk/UQbXXWnP"
 }
+
