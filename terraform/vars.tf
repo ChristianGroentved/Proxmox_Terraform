2b1de47d66498eb2a8b90725f92310c86f598718
Christian Grøntved Andersen <christian.groentved@gmail.com>
2021-10-02 21:13:24 +0200

    Correction to vars file

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -11,6 +11,6 @@ variable "pm_password" {
 }
 
 variable "ssh_key" {
-  default = "ssh-rsa WZADxvO5G19+6mAlEXOCScwvQDagfrMOonMYYUs024H3c60oqh3BvL8FGuphldPCgIfjkMfOo16yPssq63Gcj+LO7JOl4EAeDpbc6F2WTxoZGK0UuW6+mv5Btxzp/nMOn8ZZFjDzOrNY/6k6bYKLuVrzopIsMIk/UQbXXWnP"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9LTfT5vVTICTLhGjRJGlldMi6c0D3zp73KJAvWXQQtSDODtYeZlU1LE2n4/0/jxaA8Q1Inp4V5reT2PM3yjVdjHajRF7mPS8aj9n2xvYZvsy8P4PyX+9NjX7qp/CzNJWxJSjtTKIs9OK6/HYJkFxFLGQoQTL3lVLYPCkuFU6Wl6ocWZADxvO5G19+6mAlEXOCScwvQDagfrMOonMYYUs024H3c60oqh3BvL8FGuphldPCgIfjkMfOo16yPssq63Gcj+LO7JOl4EAeDpbc6F2WTxoZGK0UuW6+mv5Btxzp/nMOn8ZZFjDzOrNY/6k6bYKLuVrzopIsMIk/UQbXXWnP Kingofhkb@Christians-MBP"
 }
 
