7727fd4494ec7df513d90ed36214fa4b8be85581
Christian Grøntved Andersen <christian.groentved@gmail.com>
2022-04-15 15:40:29 +0200

    change setup to 5 nodes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,7 +17,27 @@ variable "masters" {
       gw          = "192.168.1.1"
       macaddr     = "02:DE:4D:48:28:01"
       memory      = 4096
-      disk        = "40G"
+      disk        = "30G"
+      target_node = "hkb-01"
+    },
+     k8s-master02 = {
+      id          = 4011
+      cidr        = "192.168.1.5/24"
+      cores       = 2
+      gw          = "192.168.1.1"
+      macaddr     = "02:DE:4D:48:28:02"
+      memory      = 4096
+      disk        = "30G"
+      target_node = "hkb-01"
+    },
+     k8s-master03 = {
+      id          = 4012
+      cidr        = "192.168.1.6/24"
+      cores       = 2
+      gw          = "192.168.1.1"
+      macaddr     = "02:DE:4D:48:28:03"
+      memory      = 4096
+      disk        = "30G"
       target_node = "hkb-01"
     },
   }
@@ -28,32 +48,22 @@ variable "workers" {
   default = {
     k8s-worker01 = {
       id          = 4020
-      cidr        = "192.168.1.5/24"
+      cidr        = "192.168.1.7/24"
       cores       = 2
       gw          = "192.168.1.1"
       macaddr     = "02:DE:4D:48:28:0A"
       memory      = 4096
-      disk        = "40G"
-      target_node = "hkb-01"
+      disk        = "30G"
+      target_node = "hkb-02"
     },
     k8s-worker02 = {
       id          = 4021
-      cidr        = "192.168.1.6/24"
+      cidr        = "192.168.1.8/24"
       cores       = 2
       gw          = "192.168.1.1"
       macaddr     = "02:DE:4D:48:28:0B"
       memory      = 4096
-      disk        = "40G"
-      target_node = "hkb-02"
-    },
-    k8s-worker03 = {
-      id          = 4022
-      cidr        = "192.168.1.7/24"
-      cores       = 2
-      gw          = "192.168.1.1"
-      macaddr     = "02:DE:4D:48:28:0C"
-      memory      = 4096
-      disk        = "40G"
+      disk        = "30G"
       target_node = "hkb-02"
     },
   }
