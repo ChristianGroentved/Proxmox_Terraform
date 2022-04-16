f457e8c8e9b75d3bc8b69083498cdc338adb3ed6
Christian Grøntved Andersen <christian.groentved@gmail.com>
2022-04-16 19:11:22 +0200

    only master nodes

--- terraform/workers.tf
+++ terraform/workers.tf
@@ -1,37 +1,37 @@
-resource "proxmox_vm_qemu" "kube-worker" {
-  for_each = var.workers
+# resource "proxmox_vm_qemu" "kube-worker" {
+#   for_each = var.workers
 
-  name        = each.key
-  target_node = each.value.target_node
-  agent       = 1
-  clone       = var.common.clone
-  vmid        = each.value.id
-  memory      = each.value.memory
-  cores       = each.value.cores
-  vga {
-    type = "qxl"
-  }
-  network {
-    model    = "virtio"
-    macaddr  = each.value.macaddr
-    bridge   = "vmbr0"
-    firewall = true
-  }
-  disk {
-    type    = "scsi"
-    storage = "local-lvm"
-    size    = each.value.disk
-    iothread = 1
-  }
-  serial {
-    id = 0
-    type = "socket"
-  }
-  bootdisk     = "scsi0"
-  scsihw       = "virtio-scsi-pci"
-  os_type      = "cloud-init"
-  ipconfig0    = "ip=${each.value.cidr},gw=${each.value.gw}"
-  ciuser       = "ubuntu"
-  cipassword   = data.sops_file.secrets.data["k8s.user_password"]
-  sshkeys      = data.sops_file.secrets.data["k8s.ssh_key"]
-}
+#   name        = each.key
+#   target_node = each.value.target_node
+#   agent       = 1
+#   clone       = var.common.clone
+#   vmid        = each.value.id
+#   memory      = each.value.memory
+#   cores       = each.value.cores
+#   vga {
+#     type = "qxl"
+#   }
+#   network {
+#     model    = "virtio"
+#     macaddr  = each.value.macaddr
+#     bridge   = "vmbr0"
+#     firewall = true
+#   }
+#   disk {
+#     type    = "scsi"
+#     storage = "local-lvm"
+#     size    = each.value.disk
+#     iothread = 1
+#   }
+#   serial {
+#     id = 0
+#     type = "socket"
+#   }
+#   bootdisk     = "scsi0"
+#   scsihw       = "virtio-scsi-pci"
+#   os_type      = "cloud-init"
+#   ipconfig0    = "ip=${each.value.cidr},gw=${each.value.gw}"
+#   ciuser       = "ubuntu"
+#   cipassword   = data.sops_file.secrets.data["k8s.user_password"]
+#   sshkeys      = data.sops_file.secrets.data["k8s.ssh_key"]
+# }
